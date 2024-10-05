111beb02a0443e07c8e9060ab3c1508c2fbf919a
Yufri Luttu <yufridonluttu@gmail.com>
2024-10-05 11:42:16 +0800

    bug fix dashboard admin

--- admin/index.php
+++ admin/index.php
@@ -12,26 +12,30 @@ $_SESSION['menu'] = 'index';
             <div class="card-header py-4 d-flex flex-row align-items-center justify-content-between">
                 <div class="justify-content-center text-center p-5">
                     <h5 class="text-center mb-5">
-                        SISTEM PENDUKUNG KEPUTUSAN PEMILIHAN LEMARI
+                        SISTEM PENDUKUNG KEPUTUSAN PENENTUAN PENERIMA RUMAH
+                        BANTUAN
                     </h5>
                     <p>
-                        Sistem pendukung keputusan dinyatakan pertama kali oleh Michael S.
-                        Scott Morton pada tahun 1970 dengan istilah lain “Management
-                        Decision System” SPK dibuat untuk mendukung tahap pengambilan
-                        keputusan diawali dengan mengidentifikasi masalah, memilih data
-                        relevan, menentukan pendekatan yang digunakan dalam proses
-                        pengambilan keputusan, sampai mengevaluasi pemilihan alternatif.
+                        Sistem pendukung keputusan dinyatakan pertama kali oleh Michael S. Scott
+                        Morton pada tahun 1970 dengan istilah lain “Management Decision System” SPK
+                        dibuat untuk mendukung tahap pengambilan keputusan diawali dengan
+                        mengidentifikasi masalah, memilih data relevan, menentukan pendekatan yang
+                        digunakan dalam proses pengambilan keputusan, sampai mengevaluasi pemilihan
+                        alternatif.
                     </p>
                     <p>
-                        Toko Virgo Mebel adalah salah satu dari sekian banyak
-                        toko/perusahaan yang bergerak dalam bidang mebel, dimana toko ini
-                        menjual barang berupa lemari, kursi, meja dll.
+                        Menurut Schaefer (2015: 10) MAUT digunakan untuk merubah dari beberapa
+                        kepentingan kedalam nilai numerik dengan skala 0-1 dengan 0 mewakili pilihan
+                        terburuk dan 1 terbaik. Hal ini memungkinkan perbandingan langsung beragam
+                        nilai dengan tepat. Hasil akhirnya adalah urutan peringkat dari evaluasi
+                        alternatif yang menggambarkan pilihan dari para pembuat keputusan (Lestari,
+                        2018).
                     </p>
                     <p>
-                        Toko ini memasarkan produk mulai dari kualitas standar sampai
-                        kualitas tinggi.Toko Virgo Mebel berdiri pada tanggal 6 Februari
-                        1995 dan berlokasi di Jl.Ketumbar, Kelurahan Kefamenanu Tengah,
-                        Kecamatan Kota Kefamenanu, Timor Tengah Utara Nusa Tenggara Timur.
+                        Program Bantuan Stimulan Perumahan Swadaya (BSPS) merupakan program
+                        bantuan perbaikan rumah yang dilakukan secara swadaya oleh masyarakat
+                        penerima bantuan dalam hal ini masyarakat yang terkategori Masyarakat
+                        Berpenghasilan Rendah (MBR).
                     </p>
                 </div>
             </div>
